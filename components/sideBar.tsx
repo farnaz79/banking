beb265305cf05df6d7e0ea72ee875f773636a9eb
farnazri <farnaz.zri79@gmail.com>
2024-12-07 23:50:38 +0330

    feat: create navBar link

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,8 +1,15 @@
+'use client'
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const BankSideBar = () => {
+
+  const pathName = usePathname()
+
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -18,10 +25,25 @@ const BankSideBar = () => {
             height={34}
             className="size-8 max-xl:size-12"
           />
-          <h1 className="sidebar-logo">
-            Horizin
-            </h1>
+          <h1 className="sidebar-logo">Horizin</h1>
         </Link>
+        {sidebarLinks.map((items) => {
+          const isActive = pathName === items.route || pathName
+          .startsWith(`${items.route}/`)
+          return (
+            <Link href={items.route} key={items.label} 
+            className={cn('sidebar-link' , {'bg-bank-gradient' : isActive})}>
+              <div className="relative size-6"> 
+                <Image src={items.imgURL} alt={items.label} fill 
+                className={cn({'brightness-[3] invert-0' : isActive})}
+                />
+              </div>
+              <p  className={cn('sidebar-lable text-black-1' , {'text-white' : isActive})}>
+              {items.label}
+              </p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
